e12fd2f8bd1fcc6ca704e5d51349ec4b11a15daa
notdule <148584497+notdule@users.noreply.github.com>
2024-03-02 23:47:50 +0100

    Add a Comment

--- Assets/Scripts/JokeGenerator.cs
+++ Assets/Scripts/JokeGenerator.cs
@@ -6,7 +6,7 @@ public class JokeGenerator : MonoBehaviour
 {
     void Start()
     {
-        
+        // Implement Code Thst'll randomly choose anjoke that'll be displayed when the player enters a Joke Room 
     }
 
     void Update()
